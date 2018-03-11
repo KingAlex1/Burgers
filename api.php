15a2bff9bf8389517df01cc26070e0821d3e74bc
Alex B <alex-sert2015@mail.ru>
2018-03-11 19:01:41 +0300

    fixed mistakes

--- api.php
+++ api.php
@@ -1,11 +1,13 @@
 <?php
+include_once 'keys.php';
 
 function pdoConnect()
 {
     static $pdo;
-
-    $dsn = "mysql:host=localhost;dbname=test;charset=utf8";
-    $pdo = new PDO($dsn, 'root', 'mars100');
+    if ($pdo === null) {
+        $dsn = "mysql:host=localhost;dbname=test;charset=utf8";
+        $pdo = new PDO($dsn, 'root', 'mars100');
+    }
     return $pdo;
 }
 
@@ -14,6 +16,66 @@ function pdoQuery($sql, $params = [])
     $pdo = pdoConnect();
     $prepare = $pdo->prepare($sql);
     $prepare->execute($params);
-
     return $prepare;
+}
+
+function clearDataBeforeInsert($data)
+{
+    $keys = [
+        'name',
+        'phone',
+        'email',
+        'street',
+        'home',
+        'part',
+        'aprt',
+        'floor',
+        'comment',
+        'payment'
+    ];
+
+    $result = [];
+    foreach ($keys as $value) {
+        if ($value == 'payment') {
+            $result[$value] = (!empty($data[$value])) ? "Не перезванимать" : "Перезвонить";
+        } elseif ($value == 'comment') {
+            $result[$value] = (!empty($data[$value])) ? "Потребуется сдача" : "Оплата картой";
+        } else {
+            $result[$value] = (!empty($data[$value])) ? trim($data[$value]) : null;
+        }
+    }
+    return $result;
+}
+
+function authorization($email,$sqlMailId, $sqlId )
+{
+    if (!empty($email)) {
+        $prepare = pdoQuery($sqlMailId);
+        $findMail = $prepare->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($findMail as $item) {
+            foreach ($item as $key) {
+                if ($key == $email) {
+                    echo "Вы уже зарегистрированы";
+                    $prepare = pdoQuery($sqlId , ['email' => $key]);
+                    $findId = $prepare->fetch(PDO::FETCH_ASSOC);
+                    $id = $findId['id'];
+                    return $id;
+                    break 2;
+
+
+                }
+            }
+        }
+    }
+}
+
+function checkId($id, $sql, $params = [])
+{
+    if (!$id) {
+        $prepare = pdoQuery($sql, $params);
+        $findId = $prepare->fetch(PDO::FETCH_ASSOC);
+        $id = $findId['id'];
+        return $id;
+
+    }
 }
--- order.php
+++ order.php
@@ -1,59 +1,24 @@
 <?php
 include_once('api.php');
 
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-$street = $_POST['street'];
-$home = $_POST['home'];
-$part = $_POST['part'];
-$aprt = $_POST['aprt'];
-$floor = $_POST['floor'];
-$comment = $_POST['comment'];
-$payment = $_POST['payment'] == "on" ? "Потребуется сдача" : "Оплата картой";
-$callback = isset($_POST['callback']) ? "Не перезвонить" : "Перезвонить";
+$data = clearDataBeforeInsert($_POST);
+$id = authorization($data['email'], "SELECT email, id FROM clients ", "SELECT id FROM clients WHERE email = :email");
+$prepare = pdoQuery("INSERT INTO clients (name, email,phone) VALUES (:name, :email,:phone )", ['name' => $data['name'], 'email' => $data['email'], 'phone' => $data['phone']]);
 
-if (!empty($email)) {
-    $prepare = pdoQuery("select email, id from clients");
-    $findMail = $prepare->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($findMail as $item) {
-        foreach ($item as $key) {
-            if ($key == $email) {
-                echo "Вы уже зарегистрированы";
-                $prepare = pdoQuery("select id from clients WHERE email = :email", ['email' => $key]);
-                $findId = $prepare->fetch(PDO::FETCH_ASSOC);
-                $id = $findId['id'];
-                break 2;
-            }
-        }
-    }
-}
-
-$prepare = pdoQuery("INSERT INTO clients (name, email,phone) VALUES (:name, :email,:phone )", ['name' => $name, 'email' => $email, 'phone' => $phone]);
-
-if (!$id) {
-    $prepare = pdoQuery("select id from clients WHERE email = :email", ['email' => $email]);
-    $findId = $prepare->fetch(PDO::FETCH_ASSOC);
-    $id = $findId['id'];
-    $newId = $id;
-}
+$arrID = checkId($id, "SELECT id FROM clients WHERE email = :email", ['email' => $data['email']]);
+$arrID ? $id = $arrID : null;
+$arrID ? $newId = $arrID : null;
 
 $prepare = pdoQuery("INSERT INTO burgers (user_id, street, home,part,aprt,floor,comment,payment,callback) 
 VALUES (:user_id, :street, :home, :part, :aprt, :floor, :comment, :payment, :callback)",
-    ['user_id' => $id, 'street' => $street, 'home' => $home, 'part' =>
-    $part, 'aprt' => $aprt, 'floor' => $floor, 'comment' => $comment, 'payment' => $payment, 'callback' => $callback]);
-$prepare = pdoQuery("select id , street , home , part , aprt , floor  from burgers WHERE user_id = :user_id ",['user_id' => $id]);
+    ['user_id' => $id, 'street' => $data['street'], 'home' => $data['home'], 'part' =>
+        $data['part'], 'aprt' => $data['aprt'], 'floor' => $data['floor'], 'comment' =>
+        $data['comment'], 'payment' => $data['payment'], 'callback' => $data['callback']]);
+$prepare = pdoQuery("SELECT id , street , home , part , aprt , floor  FROM burgers WHERE user_id = :user_id ", ['user_id' => $id]);
 $dataClient = $prepare->fetchAll(PDO::FETCH_ASSOC);
 $data = $dataClient[count($dataClient) - 1];
 
-$order = $data['id'];
-$street = $data['street'];
-$home = $data['home'];
-$part = $data['part'];
-$aprt = $data['aprt'];
-$floor = $data['floor'];
-
-$prepare = pdoQuery("select user_id from burgers WHERE user_id = $id");
+$prepare = pdoQuery("SELECT user_id FROM burgers WHERE user_id = $id");
 $orders = $prepare->fetchAll(PDO::FETCH_ASSOC);
 $numOrders = count($orders);
 
@@ -61,13 +26,24 @@ isset($newId) ? $str = 'Спасибо это Ваш первый заказ' : 
 
 $subject = "order";
 $message = "
-  <p>Заказ номер $order </p>
-  <p>Ваш заказ будет доставлен по адресу: улица: $street дом: $home корпус: $part квартира: $aprt этаж: $floor  </p>
+  <p>Заказ номер {$data['id']} </p>
+  <p>Ваш заказ будет доставлен по адресу: улица: {$data['street']} дом: {$data['home']} корпус:  {$data['part']} квартира: {$data['aprt']} этаж: {$data['floor']}  </p>
   <p>DarkBeefBurger за 500 рублей, 1 шт</p> 
   <p>$str</p>   
 ";
 echo $message;
-mail($email, $subject, $message);
+mail($data['email'], $subject, $message);
+
+$prepare = pdoQuery("SELECT id FROM clients ");
+$clients = $prepare->fetchAll(PDO::FETCH_ASSOC);
+$numClients = count($clients);
+$prepare = pdoQuery("SELECT id FROM burgers");
+$burgs = $prepare->fetchAll(PDO::FETCH_ASSOC);
+$allBurgers = count($burgs);
+
+echo "Всего клиентов: " . $numClients . "<br>";
+echo "Всего заказов:" . $allBurgers . "<br>";
+
 
 
 
